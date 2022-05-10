2979c0359b2c835373fc5d4c42692779c7d1400a
DALeibovich <105238549+DALeibovich@users.noreply.github.com>
2022-05-10 14:47:02 -0300

    push

---
+++ script.js
@@ -0,0 +1,20 @@
+/*var nombre = "david";
+console.log(nombre);
+
+nombre = 4;
+console.log(nombre);
+
+let sueldo = 100;
+sueldo = 400;
+console.log(sueldo);
+const IVA = 1.21;
+console.log(typeof(nombre) == "number");
+console.log(IVA * 100);
+
+console.log(-10/0*7);*/
+let numero = parseInt(prompt("ingrese un numer"));
+let numero1 = parseFloat(prompt("ingrese un numer"));
+let numero2 = Number(prompt("ingrese un numer"));
+console.log(numero);
+console.log(numero1);
+console.log(numero2);
